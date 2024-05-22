e2e4b0dfafc9553a874a70c36ef2138b68491f6b
deathscythe <digital1808ta@gmail.com>
2024-05-22 15:51:58 +0100

    .

--- README.txt
+++
@@ -1,2 +0,0 @@
-username: gaza
-password: 1234
--- js/main.js
+++ js/main.js
@@ -1,9 +1,3 @@
-let userName = "gaza";
-let password = 1234;
-let email = "something@google.com";
-let count = 2;
-
-
 document.getElementById('toggleSignup').addEventListener('click', function(e) {
     e.preventDefault();
     document.getElementById('login-form').classList.add('hidden');
@@ -16,14 +10,3 @@ document.getElementById('toggleLogin').addEventListener('click', function(e) {
     document.getElementById('login-form').classList.remove('hidden');
 });
 
-
-
-document.getElementById('login-btn').addEventListener('click', function(){
-    if(document.getElementById('usernameInput').value == userName && document.getElementById('passwordInput').value == password){
-        window.location.href = 'Save_Gaza.html';
-        return;
-    }
-    if(!count)
-        window.close();
-    count--;
-});
